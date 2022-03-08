35664e7800c09187266ff8047b8ef51b3292e5e1
FlavioAngelini <97698028+FlavioAngelini@users.noreply.github.com>
2022-03-08 19:18:00 +0100

    Add files via upload

---
+++ Ejercicio5.java
@@ -0,0 +1,9 @@
+package Tarea4;
+
+public class Ejercicio5 {
+	public static void main(String[] arg) {
+		for (int i = 0 ; i <= 100 ; i+=2) {
+			System.out.println("los pares son: "+i);
+		}
+	}
+}
